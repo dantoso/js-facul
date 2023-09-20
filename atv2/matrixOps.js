f15d99f042c8c5f34af3ae0d5e5b09b9f7e803fc
dantoso <80010223+dantoso@users.noreply.github.com>
2023-09-20 11:11:18 -0300

    add atv2

---
+++ atv2/matrixOps.js
@@ -0,0 +1,15 @@
+function transpose(matrix) {
+    let transposition = [[]];
+
+    for(i = 0; i < matrix.length; i++) {
+        for(j = 0; j < matrix[i].length; j++) {
+            
+        }
+    }
+
+    return transposition;
+}
+
+function multiply(a, b) {
+
+}
