747719153eaf9c2a539d852e6c64b90332dd7d24
dantoso <80010223+dantoso@users.noreply.github.com>
2023-09-27 10:40:08 -0300

    transposition

--- atv2/matrixOps.js
+++ atv2/matrixOps.js
@@ -1,9 +1,12 @@
 function transpose(matrix) {
-    let transposition = [[]];
+    let transposition = [];
+    for(j = 0; j < matrix[0].length; j++) {
+        transposition.push(new Array(matrix.length));
+    }
 
     for(i = 0; i < matrix.length; i++) {
         for(j = 0; j < matrix[i].length; j++) {
-            
+            transposition[j][i] = matrix[i][j];
         }
     }
 
