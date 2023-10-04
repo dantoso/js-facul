ebad82ab67370f17b1991d249d18567c9859486d
dantoso <80010223+dantoso@users.noreply.github.com>
2023-10-04 10:07:14 -0300

    feat: calculator module

--- atv4/index.js
+++ atv4/index.js
@@ -1,48 +1,32 @@
 const express =  require('express');
+const calculator = require('./util/calculator');
 const app = express();
 
 app.get("/somar/:a/:b", function (req, res) {
     let a = req.params.a;
     let b = req.params.b;
-    res.send(`${a} + ${b} = ${sum(a, b)}`);
+    res.send(`${a} + ${b} = ${calculator.sum(a, b)}`);
 });
 
 app.get("/multiplicar/:a/:b", function (req, res) {
     let a = req.params.a;
     let b = req.params.b;
-    res.send(`${a} * ${b} = ${multiply(a, b)}`);
+    res.send(`${a} * ${b} = ${calculator.multiply(a, b)}`);
 });
 
 app.get("/subtrair/:a/:b", function (req, res) {
     let a = req.params.a;
     let b = req.params.b;
-    res.send(`${a} - ${b} = ${subtract(a, b)}`);
+    res.send(`${a} - ${b} = ${calculator.subtract(a, b)}`);
 });
 
 app.get("/dividir/:a/:b", function (req, res) {
     let a = req.params.a;
     let b = req.params.b;
-    res.send(`${a} / ${b} = ${divide(a, b)}`);
+    res.send(`${a} / ${b} = ${calculator.divide(a, b)}`);
 });
 
 const PORT = 8080;
 app.listen(PORT, function () {
     console.log("app rodando na porta "+PORT);
 });
-
-
-function sum(a, b) {
-    return Number.parseFloat(a) + Number.parseFloat(b);
-}
-
-function multiply(a, b) {
-    return Number.parseFloat(a) * Number.parseFloat(b);
-}
-
-function subtract(a, b) {
-    return Number.parseFloat(a) - Number.parseFloat(b);
-}
-
-function divide(a, b) {
-    return Number.parseFloat(a) / Number.parseFloat(b);
-}
---
+++ atv4/util/calculator.js
@@ -0,0 +1,17 @@
+module.exports = {
+    sum: function(a, b) {
+        return Number.parseFloat(a) + Number.parseFloat(b);
+    },
+
+    multiply: function(a, b) {
+        return Number.parseFloat(a) * Number.parseFloat(b);
+    },
+
+    subtract: function(a, b) {
+        return Number.parseFloat(a) - Number.parseFloat(b);
+    },
+
+    divide: function(a, b) {
+        return Number.parseFloat(a) / Number.parseFloat(b);
+    },
+}
