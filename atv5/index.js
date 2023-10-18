74dde58266c2bc80e750a7c25c1fc06cda6a4ebd
dantoso <80010223+dantoso@users.noreply.github.com>
2023-10-18 10:32:53 -0300

    feat: atv5

--- atv5/index.js
+++ atv5/index.js
@@ -1,4 +1,4 @@
-const express =  require('express');
+const express = require('express');
 const app = express();
 const mustacheExpress = require('mustache-express');
 
@@ -8,20 +8,22 @@ app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extOutput: true}));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/views/index.html");
+    res.render("index.html");
 });
 
-app.post("/dados", urlEncodedParser, function (req, res) {
-    const name = req.body.nameOutput
-    const address = req.body.addressOutput
-    const phone = req.body.phoneOutput
-    const date = req.body.dateOutput
-
-    const data = [name, address, phone, date];
-    res.send(data);
+app.post("/dados", function (req, res) {
+    let data = {
+        nameOutput: req.body.nameInput,
+        addressOutput: req.body.addressInput,
+        phoneOutput: req.body.phoneInput,
+        dateOutput: req.body.dateInput
+    }
+   
+    console.log(data);
+    res.render("dados.html", {data});
 });
 
 app.listen(PORT, function () {
